45cae4d0700b374cced9312e905d37b27ef8f20d
Luiz Carlos Pereira <lcpgeop@gmail.com>
2025-05-24 17:26:41 -0400

    Criação end poit game by id - Controller, Service, Repository e DTO

--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.repositories.GameRepository;
 
@@ -14,12 +16,23 @@ public class GameService {
     @Autowired
     GameRepository gameRepository;
 
-    public List<GameMinDTO> findAll() {
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
 
-        var resultSet = gameRepository.findAll();
-        List<GameMinDTO> dto = resultSet.stream().map(x -> new GameMinDTO(x)).toList();
-        return dto;
+        // var gameSet = gameRepository.findById(id).get();
+        // GameDTO dto = new GameDTO(gameSet);
+        // return new GameDTO(gameSet);
+        return new GameDTO(gameRepository.findById(id).get());
 
     }
     
-}
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findAll() {
+
+        var resultSet = gameRepository.findAll();
+        // List<GameMinDTO> dto = resultSet.stream().map(x -> new GameMinDTO(x)).toList();
+        // return resultSet.stream().map(x -> new GameMinDTO(x)).toList();
+        return resultSet.stream().map(GameMinDTO::new).toList();
+
+    }
+ }
